58bd5fe9df7b11436115f0592ae7fbf678c6aecd
Tomilola-ng <tee.o2809@gmail.com>
2023-09-07 17:50:32 -0700

    My First C Code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - Using the puts function to print a string
- *
- * Return: always 0
- */
+	* main - print the string using the puts function
+	* Description: using the main function
+	* this program prints "Programming is like building a multilingual puzzle
+	* Return: 0
+*/
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle);
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
