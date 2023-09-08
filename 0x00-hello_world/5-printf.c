3c5ac74cb1f090fbc41a79a9532fe82f8d677483
Tomilola-ng <tee.o2809@gmail.com>
2023-09-07 18:07:14 -0700

    Push codes by Tomi :)

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+	* main - Main function
+	* Description: It uses the printffunction to print a string
+	* Return: always 0
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
