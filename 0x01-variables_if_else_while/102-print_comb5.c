adfe39e33bc88166392493d47e56b5e3a315a690
Oluwafemi Tomilola <93817917+Tomilola-ng@users.noreply.github.com>
2023-09-09 02:25:40 +0100

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,34 +1,42 @@
 #include <stdio.h>
 
+/**
+ * main - Prints 3 combination of numbers
+ * Return: Always (Success)
+ */
+
 int main(void)
 {
-    int i, j;
-
-    for (i = 0; i <= 99; i++)
-    {
-        for (j = i + 1; j <= 99; j++)
-        {
-            /* Print the first two-digit number */
-            putchar((i / 10) + '0'); /* Print tens digit */
-            putchar((i % 10) + '0'); /* Print ones digit */
-
-            /* Print a space between the two numbers */
-            putchar(' ');
-
-            /* Print the second two-digit number */
-            putchar((j / 10) + '0'); /* Print tens digit */
-            putchar((j % 10) + '0'); /* Print ones digit */
-
-            if (i != 98 || j != 99)
-            {
-                /* Print a comma and a space if it's not the last combination */
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
-
-    putchar('\n'); /* Print a new line */
-     return (0);
+int c, i, k, j;
+for (c = 48; c <= 57; c++)
+{
+	for (i = 48; i <= 57; i++)
+	{
+		for (k = 48; k <= 57; k++)
+		{
+			for (j = 48; j <= 57; j++)
+			{
+				if (((k + j) > (c + i) &&  k >= c) || c < k)
+				{
+					putchar(c);
+					putchar(i);
+					putchar(' ');
+					putchar(k);
+					putchar(j);
+				if (c + i + k + j == 227 && c == 57)
+				{
+				break;
+				}
+				else
+				{
+				putchar(',');
+				putchar(' ');
+				}
+				}
+			}
+		}
+	}
 }
-
+putchar('\n');
+return (0);
+}
