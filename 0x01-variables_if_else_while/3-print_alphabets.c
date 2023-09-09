a0430b13cd9bbb94affcc78eb1d44ac1bd7692cc
Oluwafemi Tomilola <93817917+Tomilola-ng@users.noreply.github.com>
2023-09-09 01:27:36 +0100

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,15 +10,14 @@ int main(void)
 {
 	char letter = 'a';
 
-	while (letter <= 'z') 
+	while (letter <= 'z')
 	{
-        	putchar(letter);
-	        letter++;
+		putchar(letter);
+		letter++;
 	}
 
 	letter = 'A'
-	
-	while (letter <= 'Z') 
+	while (letter <= 'Z')
 	{
 		putchar(letter);
 		letter++;
