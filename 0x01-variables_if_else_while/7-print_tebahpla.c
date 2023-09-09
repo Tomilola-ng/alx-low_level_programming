3355cc50833dfa163d3ab083d0efc43763783b37
Oluwafemi Tomilola <93817917+Tomilola-ng@users.noreply.github.com>
2023-09-09 01:59:13 +0100

    Char Int Nonsense

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,15 +1,16 @@
 #include<stdio.h>
 #include <stdlib.h>
+
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	char ch = 'z';
 	int i = 26;
-	/* your code goes there */
 	while (i > 0)
 	{
 		putchar(ch);
