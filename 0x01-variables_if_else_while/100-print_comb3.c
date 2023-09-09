74e8a33243919805ca14d9c960292f2a1b85c8dd
Oluwafemi Tomilola <93817917+Tomilola-ng@users.noreply.github.com>
2023-09-09 02:09:36 +0100

    Advanced Char Nonsense

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
 
-/**
- * main - Entry point of the program
- *
- * Return: Always 0 (Success)
- */
-
 int main(void)
 {
+    int i, j;
 
-	return (0);
+    for (i = 0; i <= 8; i++)
+    {
+        for (j = i + 1; j <= 9; j++)
+        {
+            putchar(i + '0');  /* Convert and print the first digit */
+            putchar(j + '0');  /* Convert and print the second digit */
+
+            if (i != 8 || j != 9)
+            {
+                putchar(',');   /* Print a comma */
+                putchar(' ');   /* Print a space */
+            }
+        }
+    }
+
+    putchar('\n'); /* Print a new line */
+    return (0);
 }
-
