102cab20dbce5b7c20080aa3fb18f5e71b544503
Oluwafemi Tomilola <93817917+Tomilola-ng@users.noreply.github.com>
2023-09-09 01:44:30 +0100

    Char Craze

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,30 +2,23 @@
 
 /**
  * main - Entry point of the program
- * Description: Honestly I don't know
+ *
  * Return: Always 0 (Success)
  */
 
-
 int main(void)
 {
-	char letter = 'a';
-	int i = 0;
-	while (i < 26)
-	{
-		putchar(letter);
-		letter++;
-		i++;
-	}
-	letter = 'A';
-	i = 0;
-	while (i < 26)
-	{
-		putchar(letter);
-		letter++;
-		i++;
-	}
-	putchar('\n');
-	return (0);
-}
+    char letter = 'a';
 
+    while (letter <= 'z')
+    {
+        if (letter != 'e' && letter != 'q')
+        {
+            putchar(letter);
+        }
+        letter++;
+    }
+
+    putchar('\n');
+    return (0);
+}
