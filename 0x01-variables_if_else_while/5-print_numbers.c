68f40b302b98aef6422ca1d7edb0caa38b1b4b54
Oluwafemi Tomilola <93817917+Tomilola-ng@users.noreply.github.com>
2023-09-09 01:52:46 +0100

    Char Craze

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,14 +1,23 @@
-#include <stdio.h>
+#include<stdio.h>
+#include <stdlib.h>
 
 /**
- * main - Entry point of the program
- *
+ * main - Entry point
+ * Description: Buzz kill
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-
+	char letter = '0';
+	int i = 0;
+	/* your code goes there */
+	while (i < 10)
+	{
+		putchar(letter);
+		letter++;
+		i++;
+	}
+	putchar('\n');
 	return (0);
 }
-
