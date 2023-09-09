8f15e74260aa358af93999b614716f0e2bcf9db7
Oluwafemi Tomilola <93817917+Tomilola-ng@users.noreply.github.com>
2023-09-09 01:35:29 +0100

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,13 +2,29 @@
 
 /**
  * main - Entry point of the program
- *
+ * Description: Honestly I don't know
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-
+	char letter = 'a';
+	int i = 0;
+	while (i < 26)
+	{
+		putchar(letter);
+		letter++;
+		i++;
+	}
+	letter = 'A';
+	i = 0;
+	while (i < 26)
+	{
+		putchar(letter);
+		letter++;
+		i++;
+	}
+	putchar('\n');
 	return (0);
 }
 
