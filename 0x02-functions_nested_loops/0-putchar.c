d4e7972db5883697188bd4e98a1143bf6150904d
Tomilola-ng <tee.o2809@gmail.com>
2023-09-12 17:14:51 -0700

    PutChar Task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	_putchar("_putchar\n");
+	return (0);
+}
+
