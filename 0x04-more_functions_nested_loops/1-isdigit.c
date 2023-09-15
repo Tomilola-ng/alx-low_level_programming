a2d8fe394bf4407949190d429f8da5de5b7aebc3
Tomilola-ng <tee.o2809@gmail.com>
2023-09-14 21:09:22 -0700

    Push codes by Tomi :)

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * _isdigit - Entry point of the program to check if input is a digit
+ * @c: Varaible Input
+ * Return: Always 0 (Success)
+ */
+
+int _isdigit(int c)
+{
+	return (c >= 48 && c <= 57);
+}
+
