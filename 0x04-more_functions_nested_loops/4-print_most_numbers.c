b35f5d98e6d97704ef1330a1d8d66c27561b3016
Tomilola-ng <tee.o2809@gmail.com>
2023-09-14 21:48:28 -0700

    :All numbers excluding 2 & 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+
+/**
+ * main - Entry point of the program to print 0 - 9 exclusing 2 & 4
+ * Return: None
+ */
+
+void print_most_numbers(void)
+{
+
+	return (0);
+}
+
