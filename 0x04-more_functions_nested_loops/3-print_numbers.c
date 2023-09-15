41dba1c3fb2c49f592e84b434ade296dd457d2cd
Oluwafemi Tomilola <93817917+Tomilola-ng@users.noreply.github.com>
2023-09-15 05:44:33 +0100

    Print Numbers Task

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,18 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 
 /**
  * print_numbers - Entry point of the program to print int from 0 to 9
- * Return: Always 0 (Success)
+ * Return: None
  */
 
 void print_numbers(void)
 {
-	int num;
+	int ch;
 
-	for (num = 48; num < 58; num++)
+	for (ch = 48; ch < 58; ch++)
 	{
-		_putchar(num);
+		_putchar(ch);
 	}
-	_putchar("\n");
+	_putchar('\n');
 }
