f706d61da4ddef57c72ab73458b9cee04dc5c3ef
Oluwafemi Tomilola <93817917+Tomilola-ng@users.noreply.github.com>
2023-09-15 05:50:36 +0100

    Exclude 2 & 4 from all numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
 #include "main.h"
 
-
 /**
- * main - Entry point of the program to print 0 - 9 exclusing 2 & 4
+ * print_most_numbers - Entry point of the program to print 0 - 9 exclusing 2 & 4
  * Return: None
  */
 
 void print_most_numbers(void)
 {
+	int numba;
 
+	for (numba = 0; numba <= 9; numba++)
+	{
+		if (numba != 2 && numba != 4)
+		{
+			_putchar((numba % 10) + '0');
+		}
+	}
+
+	_putchar('\n');
 	return (0);
 }
 
