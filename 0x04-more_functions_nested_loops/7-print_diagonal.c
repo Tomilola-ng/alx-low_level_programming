1df342f6229c8fe8e3e945912dbf240cc12c06f6
Oluwafemi Tomilola <93817917+Tomilola-ng@users.noreply.github.com>
2023-09-15 05:56:17 +0100

    Push Codes

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagonal - program that prints diagonal line n times.
+ * @n: times diagonal line is printed.
+ * Return: no return.
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar(92);
+		if (i < (n - 1))
+    {
+			_putchar('\n');
+    }
+	}
+	_putchar('\n');
+}
