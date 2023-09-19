4d8cbea42942e72cccd13b8e30c8d15ff24ef2de
Oluwafemi Tomilola <93817917+Tomilola-ng@users.noreply.github.com>
2023-09-19 03:51:49 +0100

    5. A good engineer thinks in reverse and asks himself about the stylistic consequences of the components and systems he proposes

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,12 +1,27 @@
 #include <stdio.h>
 
 /**
- * m - Program
- * Return: None
+ * rev_string - Program to return the length of any string passed.
+ * @var_str: variable array with the characters of a word.
+ * Return: None.
  */
 
-int m(void)
+void rev_string(char *var_str)
 {
-	return (0);
-}
+	int len, counter, half;
+	char temp;
 
+	for (len = 0; var_str[len] != '\0'; len++)
+	;
+	counter = 0;
+
+	half = len / 2;
+
+	while (half--)
+	{
+		temp = var_str[len - counter - 1];
+		var_str[len - counter - 1] = var_str[counter];
+		var_str[counter] = temp;
+		counter++;
+	}
+}
