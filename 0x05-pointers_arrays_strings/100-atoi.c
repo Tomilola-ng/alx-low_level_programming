ffe06a94645f6bff978ba3d7d6398304dd9264fc
Oluwafemi Tomilola <93817917+Tomilola-ng@users.noreply.github.com>
2023-09-21 00:30:44 +0100

    Convert string

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -2,33 +2,30 @@
 #include "main.h"
 
 /**
- * _atoi - Program that converts a string to an integer.
- * @s: the string to be comverted.
- *Return: Any integer of the newly comverted string.
+ * _atoi - Program to convert a string to an integer.
+ * @s: The variable string to be converted.
+ *Return: The integer value of the converted string.
  */
 
 int _atoi(char *s)
 {
-	int var_sign = 1;
-
-	unsigned int var_num = 0;
+	int sign = 1;
+	unsigned int num = 0;
 
 	do {
 		if (*s == '-')
-    {
-      var_sign *= -1;
-    }
-      
-    else if (*s >= '0' && *s <= '9')
-    {
-			var_num = (var_num * 10) + (*s - '0');
-    }
-      
-		else if (var_num > 0)
-    {
+		{
+			sign *= -1;
+		}
+		else if (*s >= '0' && *s <= '9')
+		{
+			num = (num * 10) + (*s - '0');
+		}
+		else if (num > 0)
+		{
 			break;
-    }
+		}
 	} while (*s++);
-  
+	
 	return (num * sign);
 }
