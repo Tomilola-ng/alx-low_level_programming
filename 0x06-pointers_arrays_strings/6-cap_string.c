ba77de0b50f3542487414132c1560681f49e3106
Oluwafemi Tomilola <93817917+Tomilola-ng@users.noreply.github.com>
2023-09-22 04:36:14 +0100

    Capitalize

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * cap_string - Program to capitalize all words of a string
+ * @src: variable string refrance
+ * Return: char *src A Capitalised string
+ */
+
+char *cap_string(char *src)
+{
+	int counter = 0;
+  
+	if ((src[counter] >= 'a') && (src[counter] <= 'z'))
+	{
+		src[counter] = src[counter] - 32;
+	}
+  
+	while (src[counter] != '\0')
+	{
+		if ((src[counter] == ' ')
+		|| (src[counter] == '\t')
+		|| (src[counter] == '\n')
+		|| (src[counter] == ',')
+		|| (src[counter] == ';')
+		|| (src[counter] == '.')
+		|| (src[counter] == '!')
+		|| (src[counter] == '?')
+		|| (src[counter] == '"')
+		|| (src[counter] == '(')
+		|| (src[counter] == ')')
+		|| (src[counter] == '{')
+		|| (src[counter] == '}'))
+		{
+			if ((src[counter + 1] >= 'a') && (src[counter + 1] <= 'z'))
+			{
+				src[counter + 1] = src[counter + 1] - 32;
+			}
+		}
+    
+		counter++;
+	}
+  
+	return (src);
+}
