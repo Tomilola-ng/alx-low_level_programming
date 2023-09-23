08f6e52f9caca767ca728e1a1232788dbce402ef
Oluwafemi Tomilola <93817917+Tomilola-ng@users.noreply.github.com>
2023-09-23 04:49:22 +0100

    Copy Cat *-*

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,23 +2,23 @@
 #include "main.h"
 
 /**
- * _strcmp - Program that copies strings only
- * @s1: variable string reference
- * @s2: variable string reference
- * Return: (0) and success
- * return an integer (the remainder of the subtraction)
+ * _strcmp - Compares two strings.
+ * @s1: The first string to be compared.
+ * @s2: The second string to be compared.
+ * Return: 0 if the strings are equal, a negative value if s1 is less than s2,
+ *         or a positive value if s1 is greater than s2.
  */
 
 int _strcmp(char *s1, char *s2)
 {
 	int counter;
 
-	for (counter = 0; ((s1[counter] != '\0') || (s2[counter] != '\0')); i++)
+	for (counter = 0; ((s1[counter] != '\0') || (s2[counter] != '\0')); counter++)
 	{
 		if (s1[counter] - s2[counter] != 0)
-    {
+		{
 			return (s1[counter] - s2[counter]);
-    }
+		}
 	}
 
 	return (0);
