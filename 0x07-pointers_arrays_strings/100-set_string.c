c0db8bea0a0758a5ca0ddeeffe1a172a3006ff5c
Tomilola-ng <tee.o2809@gmail.com>
2023-09-25 19:33:15 -0700

    String Setter

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - Program that sets char value from pointer
+ * @s: pointer to pointer
+ * @to: pointer char
+ * Return: void
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
