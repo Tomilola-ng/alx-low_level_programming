a51b332ad87d420d290286a336002f7f076588a7
Tomilola-ng <tee.o2809@gmail.com>
2023-09-25 19:11:32 -0700

    Byte Searcher

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -2,26 +2,38 @@
 
 /**
  * _strpbrk - searches a string for any of a set of bytes.
- * @s: The string to search.
- * @accept: The set of bytes to search for.
- * Return: A pointer to the byte in `s` that maches `accept`,
+ * @s: pointer with the location of the string
+ * @accept: characters to check
+ * Return: A pointer to the byte in `s` that matches `accept`,
  *         or NULL if no such byte is found.
  */
+
 char *_strpbrk(char *s, char *accept)
 {
-    while (*s != '\0')
-    {
-        char *a = accept;
-        while (*a != '\0')
-        {
-            if (*s == *a)
-                return s;
-            a++;
-        }
-        s++;
-    }
-    
-    // If no matching byte is found, return NULL
-    return NULL;
+	int length;
+	int counter, index;
+
+	/* Calculate the length of string s */
+	length = 0;
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	/* Iterate through the characters in s */
+	for (counter = 0; counter <= length; counter++)
+	{
+		/* Iterate through the characters in accept */
+		for (index = 0; accept[index] != '\0'; index++)
+		{
+			if (s[counter] == accept[index])
+			{
+				return (s + counter);
+			}
+		}
+	}
+
+	/* If no matching byte is found, return NULL */
+	return (NULL);
 }
 
