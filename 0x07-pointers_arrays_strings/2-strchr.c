713175c34aa4fb3aa1f539d16e4f896c86091cba
Tomilola-ng <tee.o2809@gmail.com>
2023-09-25 18:29:37 -0700

    Filtering Algo

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strchr - Program to find a char in a string
+ * @s: the string to be filtered
+ * @c: the character as filter value
+ * Return: Pointer to the found vale or null if not
+ */
+
+char *_strchr(char *s, char c)
+{
+	int counter = 0, index;
+
+	while (s[counter] != '\0')
+	{
+		counter++;
+	}
+
+	for (index = 0; index <= 1; index++)
+	{
+		if (s[index] == c)
+		{
+			return (s + index);
+		}
+
+		else if (s[index] ==  '\0')
+		{
+			s = '\0';
+		}
+	}
+
+	return (s);
+}
