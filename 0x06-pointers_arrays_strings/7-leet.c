3307d5ac31fab0d07d8c1c83d34e9af6045ae6d7
Oluwafemi Tomilola <93817917+Tomilola-ng@users.noreply.github.com>
2023-09-27 08:48:11 +0100

    Leet Codes

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -3,39 +3,26 @@
 
 /**
  * leet - Program that encodes a string into 1337.
- * @src: variable string refrance
+ * @src: variable string reference
  * Return: char *src an encoded string into 1337.
  */
 
-char *leet(char *src)
+char *leet(char *s)
 {
-	int counter = 0;
-	char uppercase_alphabet[] = {'4', 'B', 'C', 'D', '3', 'F', 'G', 'H', 'I',
-	'J', 'K', '1', 'M', 'N', '0', 'P', 'Q', 'R', 'S', '7', 'U',
-	 'V', 'W', 'X', 'Y', 'Z'};
-	char alphabet[] = {'4', 'b', 'c', 'd', '3', 'f', 'g', 'h', 'i',
-	 'j', 'k', '1', 'm', 'n', '0', 'p', 'q', 'r',
-	  's', '7', 'u', 'v', 'w', 'x', 'y', 'z'};
-  
-	while (src[counter] != '\0')
+	int i;
+	int index;
+	char cod[] = "aAeEoOtTlL";
+	char cod_n[] = "4433007711";
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		while (((src[counter] >= 'a') && (src[counter] <= 'z'))
-		|| ((src[counter] >= 'A') && (src[counter] <= 'Z')))
+		for (index = 0; cod[index] != '\0'; index++)
 		{
-			if ((src[counter] >= 'a') && (src[counter] <= 'z'))
+			if (s[i] == cod[index])
 			{
-				src[counter] = alphabet[src[counter] - 97];
-				break;
+				s[i] = cod_n[index];
 			}
-      else
-      {
-			  src[counter] = uppercase_alphabet[src[counter] - 65];
-			  break;
-      }
 		}
-
-		counter++;
 	}
-
-	return (src);
+	return (s);
 }
