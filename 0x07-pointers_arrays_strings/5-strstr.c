14d93f949e8b346c94ac8c40393619ba9bbba080
Oluwafemi Tomilola <93817917+Tomilola-ng@users.noreply.github.com>
2023-09-27 08:20:30 +0100

    Strings Locator

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strstr - Programthat  locates a substring in a string.
+ * _strstr - Program that locates a substring in a string.
  * @haystack: The string to search within.
  * @needle: The substring to find.
  * Return: A pointer located substring in `haystack`,
@@ -12,20 +12,20 @@ char *_strstr(char *haystack, char *needle)
 {
 	int index, counter;
 
-	for (index = 0; haystack[index] != '\0'; index++)
+	for (index = 0; haystack[index] > '\0'; index++)
 	{
-		for (counter = 0; needle[counter] != '\0'; counter++)
+		for (index = counter; hackstack[index] > '\0' && needle[index - counter] > '\0'; index++)
 		{
-			if (haystack[index + counter] != needle[counter])
+			if (haystack[index] != needle[index - counter])
 			{
 				break;
 			}
 		}
-	}
-	if (needle[counter] == '\0')
-	{
-		return (haystack + index);
+		if (needle[index - counter] == '\0')
+		{
+			return (haystack + counter);
+		}
 	}
 
-	return (NULL);
+	return (0);
 }
