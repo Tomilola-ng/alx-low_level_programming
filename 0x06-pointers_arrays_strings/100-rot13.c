714aeaf20b0a7d70ebbac16889a7abb106f35e9b
Oluwafemi Tomilola <93817917+Tomilola-ng@users.noreply.github.com>
2023-09-27 08:53:41 +0100

    Encoder

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * rot13 - Progrma that encodes rot13
+ * @src: Pointer to the parsed string
+ * Return: *src
+ */
+
+char *rot13(char *src)
+{
+	int counter, index;
+
+  char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (counter = 0; src[counter] != '\0'; counter++)
+	{
+		for (index = 0; index < 52; index++)
+		{
+			if (src[counter] == data1[index])
+			{
+				src[counter] = datarot[index];
+				break;
+			}
+		}
+	}
+  
+	return (src);
+}
