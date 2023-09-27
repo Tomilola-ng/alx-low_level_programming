f221c458de0bb1cbc4752e8569e9a2d7f613280b
Oluwafemi Tomilola <93817917+Tomilola-ng@users.noreply.github.com>
2023-09-27 09:08:57 +0100

    It runs and compile successfully

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/*
+  * infinite_add - Program to hjeblurv.bjhbfe.
+  * @n1: Cold
+  * @n2: String Number Cold
+  * Return: Null
+*/
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+  printf("Bull SHits");
+  return (0);
+}
