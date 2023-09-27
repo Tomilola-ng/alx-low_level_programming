ff16231b378375cc63c12434f5213f318ba3116f
Oluwafemi Tomilola <93817917+Tomilola-ng@users.noreply.github.com>
2023-09-27 08:58:14 +0100

    Number Character Printer

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_number - Program that prints numbers chars
+ * @n: Variavle to parsed integer
+ * Return: 0
+ **/
+
+void print_number(int n)
+{
+	unsigned int num;
+  
+  num = n;
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -n;
+	}
+
+	if (num / 10 != 0)
+	{
+		print_number(num / 10);
+	}
+
+	_putchar((num % 10) + '0');
+}
