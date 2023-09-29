0143f852cd157fba6604e45489d758a49534462b
Tomilola-ng <tee.o2809@gmail.com>
2023-09-28 17:23:50 -0700

    Config

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -10,5 +10,6 @@ int _putchar(char c);
 /* Function Declaration and Prototypes */
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
 
 #endif /* MAIN_H */
