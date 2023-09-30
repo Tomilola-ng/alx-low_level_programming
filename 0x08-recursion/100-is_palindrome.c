94943e4c660ca5950b662607064b75863b442c8f
Oluwafemi Tomilola <93817917+Tomilola-ng@users.noreply.github.com>
2023-09-30 02:44:57 +0100

    TimeBomb !!! o_O

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -2,55 +2,54 @@
 #include "main.h"
 
 /**
- * is_palindrome - Profra6returns the 1 if s is a palindrome
- * @s: string to be checked
- *
- * Return: 1 if s is a palindrome, 0 otherwise
- */
-int is_palindrome(char *s)
-{
-	int flag = 1;
-
-	check(s, 0, _strlen_recursion(s) - 1, &flag);
-	return (flag);
-}
-
-/**
- * check - checks if a string is a palindrome
- * @s: string to be checked
- * @start: start index
- * @end: end index
- * @flag: flag to indicate if a string is a palindrome
- *
- * Return: void
- */
-
-void check(char *s, int start, int end, int *flag)
-{
-	if (start <= end)
-	{
-		if (s[start] == s[end])
-			*flag *= 1;
-		else
-			*flag *= 0;
-		check(s, start + 1, end - 1, flag);
-	}
-}
-
-/**
- * _strlen_recursion - calculates the length of a string
- * @s: string to be used
- *
+ * _strlen_recursion - Program that calcs the length of a string
+ * @str: string to be used
  * Return: length of the string
  */
-int _strlen_recursion(char *s)
+
+int _strlen_recursion(char *str)
 {
 	int sum = 0;
 
-	if (*s != '\0')
+	if (*str != '\0')
 	{
 		sum++;
-		sum += _strlen_recursion(s + 1);
+		sum += _strlen_recursion(str + 1);
 	}
 	return (sum);
+}
+
+/**
+ * check - SubProgam for checking Palindrome
+ * @str: pointer string to be checked
+ * @start: start index
+ * @end: end index
+ * @flag: flag to indicate if a string is a palindrome
+ * Return: void
+ */
+
+void check(char *str, int start, int end, int *flag)
+{
+	if (start <= end)
+	{
+		if (str[start] == str[end])
+			*flag *= 1;
+		else
+			*flag *= 0;
+		check(str, start + 1, end - 1, flag);
+	}
+}
+
+/**
+ * is_palindrome - Program that checks for palindrome
+ * @str: string to be checked
+ * Return: 1 if s is a palindrome, 0 otherwise
+ */
+
+int is_palindrome(char *str)
+{
+	int flag = 1;
+
+	check(str, 0, _strlen_recursion(str) - 1, &flag);
+	return (flag);
 }
