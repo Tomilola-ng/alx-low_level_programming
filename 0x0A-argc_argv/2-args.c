3f9adb0fcedf23d3f9deb3a18d1f767bd1311a0d
Tomilola-ng <tee.o2809@gmail.com>
2023-10-02 18:47:21 -0700

    Print Argument Line By Line

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
 
 /**
- * m - Program
- * Return: None
+ * main - Entry point of the program
+ * @argc: Argument count
+ * @argv: Array of Arguments parsed
+ * Return: Always 0 (Success)
  */
 
-int m(void)
+int main(int argc, char const *argv[])
 {
+	int counter;
+
+	for (counter = 0; counter <= argc; counter++)
+	{
+		printf("%s\n", argv[counter]);
+	}
+
 	return (0);
 }
 
