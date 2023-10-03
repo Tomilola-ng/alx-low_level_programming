6a688848b437280ed4c7341fa09019bd8283f896
Tomilola-ng <tee.o2809@gmail.com>
2023-10-02 18:37:34 -0700

    No Arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * @argc: Argument count
+ * @argv: Argument List
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char const *argv[])
+{
+	(void) argv;
+
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
+
