27752234d48578300a94fad5eb7242d899128358
Tomilola-ng <tee.o2809@gmail.com>
2023-10-02 18:25:39 -0700

    Argc Argv $

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * @argc: The argument count
+ * @argv: The list of all arguments
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc:
+
+	printf("%s\n", argvv[0]);
+
+	return (0);
+}
+
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,2 @@
+## 0x0A. C - argc, argv
+### This basically relates with the arguments parsed in each command
