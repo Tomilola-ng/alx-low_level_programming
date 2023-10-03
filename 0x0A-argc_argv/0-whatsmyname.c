4d1abaee42eb5632ffb76491be784b0ceba650c4
Tomilola-ng <tee.o2809@gmail.com>
2023-10-02 18:30:16 -0700

    First Argument printer

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,11 +7,11 @@
  * Return: Always 0 (Success)
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char const *argv[])
 {
-	(void) argc:
+	(void) argc;
 
-	printf("%s\n", argvv[0]);
+	printf("%s\n", argv[0]);
 
 	return (0);
 }
