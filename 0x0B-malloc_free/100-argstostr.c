d67e63e712b824af58668d0de85b67ecb85ad4b6
Oluwafemi Tomilola <93817917+Tomilola-ng@users.noreply.github.com>
2023-10-04 23:38:43 +0100

    Malloc 100-argstostr Task

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,14 +1,10 @@
 #include <stdlib.h>
 
 /**
- * argstostr - a function that concatenates
- *             all the arguments
- *
+ * argstostr - Program that supports me
  * @ac: argument counter
  * @av: argument holder
- *
- * Return: a pointer to a new string
- *         or NULL if it fails
+ * Return: Not 0 sha
 */
 
 char *argstostr(int ac, char **av)
@@ -19,7 +15,6 @@ char *argstostr(int ac, char **av)
 	if (ac == 0 || av == NULL)
 		return (NULL);
 
-	/*find length of vector + '\0' which makes it a 2d array*/
 	length = 0;
 	for (i = 0; i < ac; i++)
 	{
