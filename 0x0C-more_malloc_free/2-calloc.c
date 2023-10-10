560d87563cac76bbb5e783e5a84a4908f4dc9be8
Oluwafemi Tomilola <93817917+Tomilola-ng@users.noreply.github.com>
2023-10-10 05:26:54 +0100

    12,14,15,16,17,18,20,23 ...

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,16 +11,24 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
     char *stk_block;
     unsigned int counter;
-
+    
     if (nmemb == 0 || size == 0)
+    {
         return (NULL);
+    }
+
     stk_block = malloc(nmemb * size);
+
     if (stk_block != NULL)
     {
         for (counter = 0; counter < (nmemb * size); counter++)
-            stk_block[counter] = 0;
+            {
+                stk_block[counter] = 0;
+            }
         return (stk_block);
     }
     else
+    {
         return (NULL);
+    }
 }
