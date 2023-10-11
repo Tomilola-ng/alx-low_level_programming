68c3b78e4635d0b5aac04062f13b50ca2c81bae9
Oluwafemi Tomilola <93817917+Tomilola-ng@users.noreply.github.com>
2023-10-11 03:34:02 +0100

    HACK_STRUCT

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,17 +1,53 @@
+// #ifndef DOG
+// #define DOG
+
+// /**
+//   * struct dog - Dog object class
+//   * @name: Dog Name
+//   * @age: Dog Age
+//   * @owner: You Idiot
+// */
+
+// struct dog {
+//   char *name;
+//   float age;
+//   char *owner;
+// };
+
+// void init_dog(struct dog *d, char *name, float age, char *owner);
+// void print_dog(struct dog *d);
+
+// typedef struct dog dog_t;
+
+// dog_t *new_dog(char *name, float age, char *owner);
+
+// void free_dog(dog_t *d);
+
+// #endif
+
+
+
 #ifndef DOG
 #define DOG
 
-/**
-  * struct dog - Dog object class
-  * @name: Dog Name
-  * @age: Dog Age
-  * @owner: You Idiot
-*/
+#include <stdio.h>
+#include <stdlib.h>
 
-struct dog {
-  char *name;
-  float age;
-  char *owner;
+/**
+ * struct dog - Program for dog class
+ *
+ * @name: Name of the dog
+ *
+ * @age: Age of the dog
+ *
+ * @owner: Owner of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
