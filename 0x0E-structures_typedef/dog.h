8cbb377788c5626cd40f3c5f37c5afa069db3f87
Oluwafemi Tomilola <93817917+Tomilola-ng@users.noreply.github.com>
2023-10-11 03:34:50 +0100

    NO_HACK_WORKS

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,53 +1,17 @@
-// #ifndef DOG
-// #define DOG
-
-// /**
-//   * struct dog - Dog object class
-//   * @name: Dog Name
-//   * @age: Dog Age
-//   * @owner: You Idiot
-// */
-
-// struct dog {
-//   char *name;
-//   float age;
-//   char *owner;
-// };
-
-// void init_dog(struct dog *d, char *name, float age, char *owner);
-// void print_dog(struct dog *d);
-
-// typedef struct dog dog_t;
-
-// dog_t *new_dog(char *name, float age, char *owner);
-
-// void free_dog(dog_t *d);
-
-// #endif
-
-
-
 #ifndef DOG
 #define DOG
 
-#include <stdio.h>
-#include <stdlib.h>
-
 /**
- * struct dog - Program for dog class
- *
- * @name: Name of the dog
- *
- * @age: Age of the dog
- *
- * @owner: Owner of the dog
- */
+  * struct dog - Dog object class
+  * @name: Dog Name
+  * @age: Dog Age
+  * @owner: You Idiot
+*/
 
-struct dog
-{
-	char *name;
-	float age;
-	char *owner;
+struct dog {
+  char *name;
+  float age;
+  char *owner;
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
@@ -60,3 +24,39 @@ dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
 #endif
+
+
+
+// #ifndef DOG
+// #define DOG
+
+// #include <stdio.h>
+// #include <stdlib.h>
+
+// /**
+//  * struct dog - Program for dog class
+//  *
+//  * @name: Name of the dog
+//  *
+//  * @age: Age of the dog
+//  *
+//  * @owner: Owner of the dog
+//  */
+
+// struct dog
+// {
+// 	char *name;
+// 	float age;
+// 	char *owner;
+// };
+
+// void init_dog(struct dog *d, char *name, float age, char *owner);
+// void print_dog(struct dog *d);
+
+// typedef struct dog dog_t;
+
+// dog_t *new_dog(char *name, float age, char *owner);
+
+// void free_dog(dog_t *d);
+
+// #endif
