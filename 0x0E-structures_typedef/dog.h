7d038cd42e875b68d61a27825a46afc2700fb3e8
Oluwafemi Tomilola <93817917+Tomilola-ng@users.noreply.github.com>
2023-10-11 02:24:31 +0100

    STRUCT_DOG

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef STRUC_DOG
+#define STRUC_DOG
+
+/**
+  * struct dog - Dog object class
+  * @name: Dog Name
+  * @age: Dog Age
+  * @owner: You Idiot
+*/
+
+struct dog
+{
+  char *name;
+  float age;
+  char *owner;
+}
+
+#endif
