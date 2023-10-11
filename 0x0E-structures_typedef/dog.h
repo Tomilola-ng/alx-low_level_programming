35ef2d54fd9adc4bf88df6c9aeeb98752b4b4255
Oluwafemi Tomilola <93817917+Tomilola-ng@users.noreply.github.com>
2023-10-11 03:03:39 +0100

    STRUC_MAIN_DOG

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,13 +14,13 @@ struct dog {
   char *owner;
 };
 
-// void init_dog(struct dog *d, char *name, float age, char *owner);
-// void print_dog(struct dog *d);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
-// typedef struct dog dog_t;
+typedef struct dog dog_t;
 
-// dog_t *new_dog(char *name, float age, char *owner);
+dog_t *new_dog(char *name, float age, char *owner);
 
-// void free_dog(dog_t *d);
+void free_dog(dog_t *d);
 
 #endif
