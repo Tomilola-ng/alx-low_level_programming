b05e352f7682b22cf2adf80373ec0f79475cfacc
Tomilola-ng <tee.o2809@gmail.com>
2023-10-10 17:40:01 -0700

    C File Name Printer

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Program that prints file name
+ * Return: Always 0 (Success) or 1 (error)
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
+
