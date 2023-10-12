87dda74c29486e93c1a5e119bdb3ff240caa77e3
Oluwafemi Tomilola <93817917+Tomilola-ng@users.noreply.github.com>
2023-10-13 00:52:21 +0100

    PRINTER_OF_STR

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,35 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_strings - Program prints strings
+ * @separator: ptr str to be printed between numbers
+ * @n: int num of strings passed to the function
+ * Return: None 
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list lc;
+	unsigned int counter;
+	char *p;
+
+	va_start(lc, n);
+	for (counter = 0; counter < n; counter++)
+	{
+		p = va_arg(lc, char *);
+		if (p == NULL)
+		{
+			printf("(nil)");
+		}
+		else
+			printf("%s", p);
+		if (counter != (n - 1) && separator != NULL)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+	va_end(lc);
+}
