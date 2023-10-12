d45fb19e40a737dc829d8d8f703aeb44f9999b1c
Oluwafemi Tomilola <93817917+Tomilola-ng@users.noreply.github.com>
2023-10-13 00:48:44 +0100

    SUM_ALL

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdarg.h>
+
+/**
+ * sum_them_all - Program that adds arguements passed
+ * @n: Integer number of passed arguements
+ * Return: sum of num
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int counter;
+	int sum = 0;
+
+	va_list ap;
+
+	va_start(ap, n);
+	if (n == 0)
+		return (0);
+	for (counter = 0; counter < n; counter++)
+		sum += va_arg(ap, int);
+	va_end(ap);
+	return (sum);
+}
