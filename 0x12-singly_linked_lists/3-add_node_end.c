0962990ed05ed865f8162c0bbc73c0042c13a881
Oluwafemi Tomilola <93817917+Tomilola-ng@users.noreply.github.com>
2023-10-23 13:41:30 +0100

    ADD_NODE

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * add_node_end -  Node pops
+ * @head:FIRSTNODE Ptr
+ * @str: string ptr
+ * Return: ptr address
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *p, *tmp;
+	unsigned int node;
+
+	p = malloc(sizeof(list_t));
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+
+	p->str = strdup(str);
+
+	if (p->str == NULL)
+	{
+		free(p);
+		return (NULL);
+	}
+
+	node = 0;
+
+	while (str[node] != '\0')
+	{
+		node++;
+	}
+
+	p->len = node;
+	p->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = p;
+	}
+	else
+	{
+		tmp = *head;
+		while (tmp->next != 0)
+		{
+			tmp = tmp->next;
+		}
+		tmp->next = p;
+	}
+	return (p);
+}
