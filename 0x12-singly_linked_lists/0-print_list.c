e59d7803ed4e801c312daabdb069f0f12d131540
Oluwafemi Tomilola <93817917+Tomilola-ng@users.noreply.github.com>
2023-10-23 13:21:59 +0100

    TABS

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,18 +14,18 @@ size_t print_list(const list_t *h)
 	nodes = 0;
 
 	while (h != NULL)
+	{
+		if (h->str == NULL)
 		{
-			if (h->str == NULL)
-			{
-				printf("[0] (nil)\n");
-			}
-			else
-			{
-				printf("[%d] %s\n", h->len, h->str);
-				h = h->next;
-				nodes++;
-			}
+			printf("[0] (nil)\n");
 		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+			h = h->next;
+			nodes++;
+		}
+	}
 
 	return (nodes);
 }
