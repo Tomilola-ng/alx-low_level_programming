ae0388404ba19f526e82428a56fc2565f6c22d66
Oluwafemi Tomilola <93817917+Tomilola-ng@users.noreply.github.com>
2023-10-24 12:46:01 +0000

    POP_LIST

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,23 +1,25 @@
 #include "lists.h"
+
 /**
- * pop_listint - deletes the head node of a listint_t linked list
- * @head: pointer with the head, first node adress
- * Return: head nodes data.
+ * pop_listint - PROGRAM pops off the first
+ * @head: HEAD ptr
+ * Return: address
  */
+
 int pop_listint(listint_t **head)
 {
-	listint_t *tmp1, *tmp2;
-	int n;
+	listint_t *tmp1, *temporary;
+	int count;
 
 	tmp1 = *head;
 	if (tmp1 == NULL)
-	{
 		return (0);
-	}
-	tmp2 = *head;
+
+	temporary = *head;
 	tmp1 = tmp1->next;
 	*head = tmp1;
-	n = tmp2->n;
-	free(tmp2);
-	return (n);
+	count = temporary->n;
+	free(temporary);
+
+	return (count);
 }
