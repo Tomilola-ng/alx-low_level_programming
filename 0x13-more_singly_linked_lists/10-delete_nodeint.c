14e43d5166bcc08141df87300ef2eced7db00ade
Oluwafemi Tomilola <93817917+Tomilola-ng@users.noreply.github.com>
2023-10-24 12:57:51 +0000

    DELETE_NODES

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,35 +1,39 @@
 #include "lists.h"
+
 /**
- * delete_nodeint_at_index - deletes node at a given position.
- * @head: pointer with the first node.
- * @index: the index of the node.
- * Return: 1 if it succeeded, -1 if it failed.
+ * delete_nodeint_at_index - PROGRAM THAT deletes node
+ * @head: HEAD ptr
+ * @index: node index
+ * Return: 1 or -1
  */
+
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *temp, *temp2;
-	unsigned int i = 1;
+	listint_t *temp, *temporary;
+	unsigned int counter = 1;
 
 	temp = *head;
-	while (temp != NULL)
+
+	do
 	{
 		if (index == 0)
+		{
+			temporary = *head;
+			*head = temporary->next;
+			free(temporary);
+			return (1);
+		}
+		else if (counter == index)
+		{
+			temporary = temp->next;
+			temp->next = temporary->next;
+			free(temporary);
+			return (1);
+		}
 
-		{
-			temp2 = *head;
-			*head = temp2->next;
-			free(temp2);
-			return (1);
-		}
-		else if (i == index)
-		{
-			temp2 = temp->next;
-			temp->next = temp2->next;
-			free(temp2);
-			return (1);
-		}
 		temp = temp->next;
-		i++;
-	}
+		counter++;
+	} while (temp != NULL);
+
 	return (-1);
 }
