6bf74b7f464bb16894b230580e595b0afb6c5165
Tomilola-ng <tee.o2809@gmail.com>
2024-02-29 17:42:32 -0800

    5. Island Perimeter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+    Function to calc perimeter
+"""
+
+def island_perimeter(grid):
+    """
+        FUNC to calc with array of number as params
+    """
+    perimeter = 0
+    rows = len(grid)
+    cols = len(grid[0])
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+
+    return perimeter
+
