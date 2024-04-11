84db89b82f079c12cbefd2ed0ca88ead083965bf
Tomilola-ng <tee.o2809@gmail.com>
2024-04-11 23:24:54 +0100

    CODE_ALT

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -10,31 +10,26 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-	size_t left = 0, right = size - 1;
+	size_t idx, right, left;
 
-    if (array == NULL)
-        return (-1);
+	if (array == NULL)
+		return (-1);
 
-    printf("Searching in array: ");
-    for (size_t mid = 0; mid < size; mid++)
-        printf("%d, ", array[mid]);
-    printf("\b\b\n");
+	for (left = 0, right = size - 1; right >= left;)
+	{
+		printf("Searching in array: ");
+		for (idx = left; idx < right; idx++)
+			printf("%d, ", array[idx]);
+		printf("%d\n", array[idx]);
 
-    while (left <= right) {
-        size_t mid = left + (right - left) / 2;
+		idx = left + (right - left) / 2;
+		if (array[idx] == value)
+			return (idx);
+		if (array[idx] > value)
+			right = idx - 1;
+		else
+			left = idx + 1;
+	}
 
-        if (array[mid] == value)
-            return (mid);
-        else if (array[mid] < value)
-            left = mid + 1;
-        else
-            right = mid - 1;
-
-        printf("Searching in array: ");  // Print statements moved outside the inner loop
-        for (size_t i = left; i <= right; i++)
-            printf("%d, ", array[i]);
-        printf("\b\b\n");
-    }
-
-    return (-1);
+	return (-1);
 }
