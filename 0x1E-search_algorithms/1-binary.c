a87992d94af83032cb513abf9cf24319947a3298
Tomilola-ng <tee.o2809@gmail.com>
2024-04-11 23:17:29 +0100

    LOOP_COMPATIBILITY

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -10,38 +10,30 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-	size_t left = 0, right = size - 1, mid;
+	size_t left = 0, right = size - 1;
 
     if (array == NULL)
         return (-1);
 
     printf("Searching in array: ");
-    for (mid = 0; mid < size; mid++)
+    for (size_t mid = 0; mid < size; mid++)
         printf("%d, ", array[mid]);
     printf("\b\b\n");
 
-    while (left <= right)
-    {
-        mid = left + (right - left) / 2;
+    while (left <= right) {
+        size_t mid = left + (right - left) / 2;
 
         if (array[mid] == value)
             return (mid);
         else if (array[mid] < value)
-        {
-            printf("Searching in array: ");
-            for (size_t i = left; i <= right; i++)
-                printf("%d, ", array[i]);
-            printf("\b\b\n");
             left = mid + 1;
-        }
         else
-        {
-            printf("Searching in array: ");
-            for (size_t i = left; i <= right; i++)
-                printf("%d, ", array[i]);
-            printf("\b\b\n");
             right = mid - 1;
-        }
+
+        printf("Searching in array: ");  // Print statements moved outside the inner loop
+        for (size_t i = left; i <= right; i++)
+            printf("%d, ", array[i]);
+        printf("\b\b\n");
     }
 
     return (-1);
